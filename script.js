0cb9443d2aaf587e8af332960c3b5ed1e895059f
DivyaBhulai <37069270+DivyaBhulai@users.noreply.github.com>
2021-04-21 16:46:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 // Mastermind
 
-var kleuren = ['#fc0303', '#fc6b03', '#fce703', '#0ffc03', '#03f4fc', '#9803fc', '#fc03b1']
+var kleuren = ['#fc0303', '#fce703', '#0ffc03', '#03f4fc', '#9803fc', '#fc03b1']
+var antwoord = [];
+
+AntwoordenRij();
+
+function AntwoordenRij(){
+  // AntwoordenRij is grijs gekleurd tot de juiste kleur geraden wordt, want dan verandert het grijze vakje in de geraden kleur
+    for(var i = 0; i < 4; i++){
+        var cel = document.querySelector('#antwoord_kolom' + i);
+        //Maak random kleuren combinatie
+        var kleur = Math.floor(Math.random() * 6);
+        cel.classList.add('onbekend');
+        //Zet kleuren in list antwoord
+        antwoord.push(kleur);
+        //Check
+        alert("kleur");
+    }
+    //Ga naar funcctie PogingRij
+    PogingRij(1);
+}
+
+function PogingRij(rijNummer){
+    // Rij voor poging toevoegen + Raad! knopje maken
+
+}
